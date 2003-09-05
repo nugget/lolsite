a938e380ee696c969b2304aa2fbb66b70e4b449d
gregh <gregh>
2003-09-05 03:19:08 +0000

    add routeup script

---
+++ tools/routeup.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -w
+
+# This is used to upgrade from schema 3 to schema 4.
+# Change the DBI->connect line as appropriate.
+
+use DBI;
+
+$db = DBI->connect("DBI:Pg:dbname=lol", "lolsite");
+die unless $db;
+$s = $db->prepare("select id, route from logbook");
+$s->execute;
+while (@a = $s->fetchrow) {
+    $route = $a[1];
+    print "$route\n";
+    @r = split /\s+/, $route;
+    $t = $db->prepare("delete from flight_route where logbook_id = ?");
+    $t->execute($a[0]);
+    $seq = 0;
+    foreach (@r) {
+        $t = $db->prepare("insert into flight_route (logbook_id, airport, sequence) values (?,?,?)");
+        $t->execute($a[0], $_, $seq++);
+    }
+}
+$db->disconnect;
