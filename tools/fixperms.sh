0410d82e2b6e3580dac1b4e831cd00d7d1dc8183
nugget <nugget>
2003-10-12 18:13:06 +0000

    Changes for group editing perms.

--- tools/fixperms.sh
+++ tools/fixperms.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-find . -type d | xargs chmod 711
-find . -type d -name CVS | xargs chmod 700
-find . -type f | xargs chmod 644
-find . -type f -name "*.pl" | xargs chmod 755
-find . -type f -name "*.sh" | xargs chmod 755
-find . -type f -name "installer" | xargs chmod 755
-find ./htdocs -type f | xargs chmod 744
-find ./htdocs -type d | xargs chmod 711
+find . -type d | xargs chmod 771
+find . -type d -name CVS | xargs chmod 770
+find . -type f | xargs chmod 664
+find . -type f -name "*.pl" | xargs chmod 775
+find . -type f -name "*.sh" | xargs chmod 775
+find . -type f -name "installer" | xargs chmod 775
+find ./htdocs -type f | xargs chmod 774
+find ./htdocs -type d | xargs chmod 771
